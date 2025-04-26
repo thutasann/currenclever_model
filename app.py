071db5a8e9266b7f0a8616f7ca8297d8275f8c20
thutasann <thutasann2002@gmail.com>
2025-04-26 19:34:30 +0800

    tewak fly

--- app.py
+++ app.py
@@ -85,5 +85,5 @@ def test():
     return "Test route is working!"
 
 if __name__ == '__main__':
-    print("🚀 Starting Prediction API test...")
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
